43434eed249c5be9b6b5da2abd2e648cde1ac0ba
mariane <mariane.sp@hotmail.com>
2018-08-06 16:34:18 -0300

    adicionando classes de sessão

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,46 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+public class Sessao {
+	
+	private Integer id;
+	private LocalTime horario;
+	private Sala sala;
+	private Filme filme;
+	
+	public Sessao(LocalTime horario, Filme filme , Sala sala){
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+	}
+	
+	public LocalTime getHorarioTermino(){
+		return this.horario.plusMinutes(filme.getDuracao().toMinutes());		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+}
